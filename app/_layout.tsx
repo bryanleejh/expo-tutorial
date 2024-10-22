82187eefd5559e60306475b8c2d1471927403a52
Bryan Lee <bryan.lee.jh@gmail.com>
2024-10-22 19:37:25 +0800

    feat: configure status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,22 @@
 import { Stack } from "expo-router";
+import { setStatusBarStyle, StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
+  useEffect(() => {
+    setTimeout(() => {
+      setStatusBarStyle("light");
+    }, 0);
+  }, []);
+
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar style="light" />
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+    </GestureHandlerRootView>
   );
 }
